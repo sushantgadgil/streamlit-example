d76413b1aedb2eee9e2459029f924e24c72463e8
sushantgadgil <98911787+sushantgadgil@users.noreply.github.com>
2022-02-02 15:40:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ if display:
 st.header("Images From Your Location")
 
 with st.expander("See Media"):
-    st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     st.warning("Never Gonna Give You Up")
+    st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     st.image("https://picsum.photos/500/300")    
 
