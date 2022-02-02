d3f98be2c4324ea94448809948c8d20c65b4d1f9
sushantgadgil <98911787+sushantgadgil@users.noreply.github.com>
2022-02-02 15:12:19 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,18 +6,10 @@ import streamlit as st
 import numpy as np
 from datetime import datetime
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
 st.title("SYSEN 5160 HW 1: Streamlit Examples")
+
+st.sidebar.write("City Temp","Media")
+
 
 st.header("View a Cities' Temperature")
 
@@ -41,11 +33,11 @@ if display:
     st.line_chart(sampl)
     st.bar_chart(sampl)
 
-st.subheader("Images From Your Location")
+st.header("Images From Your Location")
 
 disp2 = st.checkbox("Show Media")
 
-if disp2:
+with st.expander("See Media")
     st.image("https://picsum.photos/200/300")
     st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     
